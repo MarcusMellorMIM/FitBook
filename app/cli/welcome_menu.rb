5cdbf1479459533ad8e197926a23f73c73ca66fc
github username <github email address>
2019-05-07 15:49:28 +0100

    testing

--- app/cli/welcome_menu.rb
+++ app/cli/welcome_menu.rb
@@ -1,5 +1,10 @@
-Anthea
-It welcomes the user. Ask them if they want to create or find a user.
-If they found a user, they will display all the
-menu options: add weight, add meal, add exercise, view summary or view daily details.
-make the last menu interactive, they can check their last weight that was entered.
+# Anthea
+# It welcomes the user. Ask them if they want to create or find a user.
+# If they found a user, they will display all the
+# menu options: add weight, add meal, add exercise, view summary or view daily details.
+# make the last menu interactive, they can check their last weight that was entered.
+
+class WelcomeMenuCLI
+
+ #create user
+end
