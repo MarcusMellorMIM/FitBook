d7dc0130ead3d913a77b1f93a01102400e5dd093
github username <github email address>
2019-05-07 12:25:39 +0100

    testing

--- app/cli/welcome_menu.rb
+++ app/cli/welcome_menu.rb
@@ -1,3 +1,4 @@
+Anthea
 It welcomes the user. Ask them if they want to create or find a user.
 If they found a user, they will display all the
 menu options: add weight, add meal, add exercise, view summary or view daily details.
