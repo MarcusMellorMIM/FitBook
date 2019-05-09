5ad81b2773514a0cd30de6c38d25806555565cd2
github username <github email address>
2019-05-09 15:36:56 +0100

    trying

--- db/Seed.rb
+++ db/Seed.rb
@@ -1,7 +1,7 @@
-Exercise_Type.create(detail:"light")
-Exercise_Type.create(detail:"moderate")
-Exercise_Type.create(detail:"intense")
+ExerciseType.create(detail:"light")
+ExerciseType.create(detail:"moderate")
+ExerciseType.create(detail:"intense")
 
-Meal_Type.create(detail:'healthy')
-Meal_Type.create(detail:'ok')
-Meal_Type.create(detail:'unhealthy')
+MealType.create(detail:'angelic')
+MealType.create(detail:'meh')
+MealType.create(detail:'guilty')
